1816ea489a6aae04cab303596e720981d275944d
Bektur777 <ubei003@mail.ru>
2022-08-19 22:19:01 +0600

    adding models to the order

--- onlineStore/settings.py
+++ onlineStore/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
     'cart.apps.CartConfig',
+    'order.apps.OrderConfig',
 
     'ckeditor',
     'ckeditor_uploader',
--- order/models.py
+++ order/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+from store.models import Product
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='Заказы', on_delete=models.CASCADE)
+    first_name = models.CharField('Имя', max_length=50)
+    last_name = models.CharField('Фамилия', max_length=50)
+    email = models.EmailField()
+    address = models.CharField('Адрес', max_length=255)
+    city = models.CharField('Город', max_length=50)
+    phone = models.IntegerField('Номер телефона')
+    paid = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='Заказ', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='Товар', on_delete=models.CASCADE)
+    price = models.IntegerField('Цена')
+    quantity = models.IntegerField('Количество', default=1)
